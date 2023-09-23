1bb0295de920161f3b524f93e63f235bf2c953f8
Joseph Karanja <joseph.karanja@student.moringaschool.com>
2023-09-23 19:45:03 +0300

    complete assignement

--- server/app.py
+++ server/app.py
@@ -14,13 +14,78 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET', 'POST'])
 def messages():
-    return ''
+    
+    if request.method == 'GET':
+        messages = Message.query.all()
+        messages_serialized = [message.to_dict() for message in messages]
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            messages_serialized,
+            200
+        )
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+    
+    elif request.method == 'POST':
+        new_message = Message(
+            body = request.form.get("body"),
+            username = request.form.get("username"),
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_dict = new_message.to_dict()
+
+        response = make_response(
+            jsonify(new_message_dict),
+            201
+        )
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+
+@app.route('/messages/<int:id>', methods = ['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).limit(1).first()
+
+    if request.method == 'PATCH':
+        for attr in request.form:
+            setattr(message, attr, request.form.get(attr))
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(
+            jsonify(message_dict),
+            200
+        )
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete successful": True,
+            "message": "Message Deleted",
+        }
+
+        response = make_response(
+            jsonify(response_body),
+            200
+        )
+        response.headers["Content-Type"] = "application/json"
+        
+        return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
